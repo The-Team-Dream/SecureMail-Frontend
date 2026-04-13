ff0bf73aefa204bdf433fc04045edbdbfb1fd4b6
Emad Ahmed <ea960183@gmail.com>
2026-04-13 17:47:09 +0200

    feat: enhance Logo component with textSize prop for dynamic text sizing

--- src/_components/shared/Logo.tsx
+++ src/_components/shared/Logo.tsx
@@ -1,17 +1,37 @@
 import Image from "next/image";
 import { Text } from "./Text";
+import { ComponentProps } from "react";
+/**
+ *  Logo Component
+ ** Props:
+ * @param width - customizable width (default: 50)
+ * @param height - customizable width (default: 50)
+ * @param textSize - dynamic text sizing (example: sm, md, lg)
+ * @param className - to add external classes if needed
+ **  Example:
+ * <Logo />
+ * <Logo width={30} height={30} textSize="lg" />
+ * <Logo width={120} height={120} textSize="6xl" className="flex-col" />
+ */
+type TextSize = ComponentProps<typeof Text>["size"];
 
 interface LogoProps {
   width?: number;
   height?: number;
   className?: string;
+  textSize?: TextSize;
 }
 
-const Logo = ({ width = 50, height = 50, className }: LogoProps) => {
+const Logo = ({
+  width = 50,
+  height = 50,
+  className,
+  textSize = "3xl",
+}: LogoProps) => {
   return (
     <div className={`flex items-center gap-2 ${className}`}>
       <Image src={"/icons/logo.png"} alt="Logo" width={width} height={height} />
-      <Text as={"h1"} font={"black"} size={"3xl"}>
+      <Text as={"h1"} font={"black"} size={textSize}>
         SecureMail
       </Text>
     </div>
